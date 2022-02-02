c6df7d5065d6f1b4f4f1256b53c2ff7b739abd88
thaleswillreis <thaleswill@yahoo.com.br>
2022-02-02 01:13:23 -0300

    DepartmentService

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,9 +1,12 @@
 package gui;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -12,8 +15,11 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentListController implements Initializable {
+	
+	private DepartmentService service;
 
 	@FXML
 	private TableView<Department> tableViewDepartment;
@@ -27,9 +33,15 @@ public class DepartmentListController implements Initializable {
 	@FXML
 	private Button btNew;
 	
+	private ObservableList<Department> obsList;//lista para carregar os departamentos que serao mostrados atraves do metodo setDepartmentService
+	
 	@FXML
 	public void onBtNewAction() {
 		System.out.println("onBtNewAction");//só testando o botao
+	}
+	
+	public void setDepartmentService(DepartmentService service) {//metodo para injetar a dependencia sem cria um acoplamento forte
+		this.service = service;
 	}
 	
 	@Override
@@ -47,4 +59,12 @@ public class DepartmentListController implements Initializable {
 		
 	}
 
+	public void updateTableView() {//acessar os servicos, carregar os departments e joga-los na ObservableList
+		if (service == null) {//testa e preveni que o programador esqueça de injetar a dependencia
+			throw new IllegalStateException("Service was null");
+		}
+		List<Department> list = service.findAll();//Lista que recebe os departamentos Mockados
+		obsList = FXCollections.observableArrayList(list);//carraga a lista dentro do obsList
+		tableViewDepartment.setItems(obsList);//carrega os itens e mostra na tela
+	}
 }
--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -15,6 +15,7 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
+import model.services.DepartmentService;
 
 public class MainViewController implements Initializable {
 
@@ -29,12 +30,12 @@ public class MainViewController implements Initializable {
 	public void onMenuItemSellerAction() {
 		System.out.println("onMenuItemSellerAction");
 	}
-
+//========================= ATENCAO ========================
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		loadView("/gui/DepartmentList.fxml");
+		loadView2("/gui/DepartmentList.fxml");//loadView2 == loadView modificado para teste ... desfazer depois
 	}
-
+//==========================================================
 	@FXML
 	public void onMenuItemAboutAction() {
 		loadView("/gui/About.fxml");
@@ -64,4 +65,25 @@ public class MainViewController implements Initializable {
 		}
 	}
 
+	private synchronized void loadView2(String absoluteName) {//manipulando a cena principal para incluir alem do mainMenu os filhos da janela que for aberta (criacao da janela)
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
+			
+			Scene mainScene = Main.getMainScene();//recebe o return com a referencia da scene
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();//variavel do tipo VBox recebendo um casting do Vbox no casting do getRoot para o ScrollPane
+			
+			Node mainMenu = mainVBox.getChildren().get(0);//guarda referencia para o menu numa var do tipo Node
+			mainVBox.getChildren().clear();//limpa todos os filhos do mainVBox para os substituirmos
+			mainVBox.getChildren().add(mainMenu); //adiciona o mainMenu nos filhos do mainVBox que limpamos
+			mainVBox.getChildren().addAll(newVBox.getChildren());//adiciona a colecao com os filhos do newVBox
+		
+			DepartmentListController controller = loader.getController();//o mesmo objeto 'loader' carrega a view e acessa o controller
+			controller.setDepartmentService(new DepartmentService());//injeta as dependencias do services no controller
+			controller.updateTableView();//atualiza os dados na tela da tebleView
+		}
+		catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
+	}
 }
